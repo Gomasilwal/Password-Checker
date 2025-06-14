b83bc61a4386db110599345ad48b30e77480435d
GomaSilwal <gomasilwal9@gmail.com>
2025-06-15 01:00:11 +0545

    message for test class as strength on JS

--- script.js
+++ script.js
@@ -9,6 +9,21 @@ function checkStrength() {
     if(/[0-9]/.test(pwd)) strength++;
     if(/[^A-Za-z0-9]/.test(pwd)) strength++;
 
+//Reset Class
+    msg.className = "strength";
 
+    if (strength === 5){
+        msg.testContent = "Very Strong Password";
+        msg.classList.add("very-strong");
+    } else if (strength >= 4){
+         msg.testContent = "Strong Password";
+        msg.classList.add("strong");
+    }else if (strength >= 3){
+         msg.testContent = "Moderate Password";
+        msg.classList.add("moderate");
+    }else {
+         msg.testContent = "Weak Password";
+        msg.classList.add("weak");
+    }
     
 }
